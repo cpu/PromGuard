7a7f2ae7d11c0db3b7b5ef2b80119a16a1f68d5b
Daniel <daniel@binaryparadox.net>
2018-01-28 20:48:31 -0500

    Comment terraform code

--- promguard.tf
+++ promguard.tf
@@ -1,27 +1,55 @@
+/*
+ * PromGuard Terraform
+ * @cpu - 2018
+ *
+ * An example of creating 1 monitoring node, and n target nodes. Target nodes
+ * are spread across geographically diverse regions. Template datasources are
+ * used to populate an Ansible inventory output. Later Ansible playbooks will
+ * create an encrypted site-to-site tunnel between the monitoring node and the
+ * target nodes.
+ *
+ */
+
+/*
+ * Placeholder variables - populate these in your `terraform.tfvars` file.
+ */
 variable "do_token" {}
-
 variable "do_ssh_key_file" {}
-
 variable "do_ssh_key_name" {
-  default = "promguard ssh key"
+  default = "promguard-ssh"
 }
 
-variable "node_count" {
-  default = 3
-}
+/*
+ * PromGuard Configuration Options
+ */
 
+# What size of Droplet should be used?
 variable "do_size" {
   default = "1gb"
 }
 
+# What droplet image should be used?
 variable "do_image" {
   default = "ubuntu-16-04-x64"
 }
 
+# What region should the monitoring droplet be in?
+# Default: Toronto, CA
 variable "do_monitor_region" {
   default = "tor1"
 }
 
+# How many nodes should be created - make sure `do_node_regions` has an entry
+# for each `node_count` if you change this!
+variable "node_count" {
+  default = 3
+}
+
+# What regions should each of the remote nodes be created in?
+# Default:
+#  Node 1 - London, UK
+#  Node 2 - San Franciso, US
+#  Node 3 - Singapore, SG
 variable "do_node_regions" {
   type = "map"
 
@@ -32,6 +60,12 @@ variable "do_node_regions" {
   }
 }
 
+/*
+ * Template Data Sources
+ */
+
+# nodes_ansible is a template datasource that constructs a hostname template for
+# each of the "node" droplets
 data "template_file" "nodes_ansible" {
   count = "${var.node_count}"
   template = "${file("${path.module}/templates/hostname.tpl")}"
@@ -42,6 +76,8 @@ data "template_file" "nodes_ansible" {
   }
 }
 
+# monitor_ansible is a template datasource that constructs a hostname template
+# only for the "monitor" droplet
 data "template_file" "monitor_ansible" {
   template = "${file("${path.module}/templates/hostname.tpl")}"
   vars {
@@ -51,6 +87,8 @@ data "template_file" "monitor_ansible" {
   }
 }
 
+# ansible_inventory is a template datasource that stitches together the
+# nodes_ansible and monitor_ansible datasources to make an Ansible inventory
 data "template_file" "ansible_inventory" {
   template = "${file("${path.module}/templates/inventory.tpl")}"
   vars {
@@ -59,39 +97,51 @@ data "template_file" "ansible_inventory" {
   }
 }
 
+# ansible_inventory is an output for the rendered ansible_inventory template
 output "ansible_inventory" {
   value = "${data.template_file.ansible_inventory.rendered}"
 }
 
+# monitor_ip is an output for the public IPv4 address of the monitor droplet
+# used for SSH port forwarding
 output "monitor_ip" {
   value = "${digitalocean_droplet.monitor.ipv4_address}"
 }
 
+# Use DigitalOcean as the provider
 provider "digitalocean" {
   token = "${var.do_token}"
 }
 
+# Create an SSH key for the PromGuard droplets to reference
 resource "digitalocean_ssh_key" "promguard-ssh" {
   name = "${var.do_ssh_key_name}"
   public_key = "${file(var.do_ssh_key_file)}"
 }
 
+# Create an overall "promguard" tag that all droplets will reference
 resource "digitalocean_tag" "promguard" {
   name = "promguard"
 }
 
+# Create a "promguard_monitor" tag that only the monitor host will reference
 resource "digitalocean_tag" "promguard_monitor" {
   name = "promguard_monitor"
 }
 
+# Create a "promguard_node" tag that only the to-be-monitored nodes will
+# reference
 resource "digitalocean_tag" "promguard_node" {
   name = "promguard_node"
 }
 
+# Create a single "monitor" droplet in the do_monitor_region
+# A "remote-exec" provisioner is used to ensure SSH access is available before
+# continuing with further provisioning stages.
 resource "digitalocean_droplet" "monitor" {
-  image = "${var.do_image}"
   name = "promguard-monitor-1"
   region = "${var.do_monitor_region}"
+  image = "${var.do_image}"
   size = "${var.do_size}"
   ssh_keys = [ "${digitalocean_ssh_key.promguard-ssh.id}" ]
   tags = [ "${digitalocean_tag.promguard.id}", "${digitalocean_tag.promguard_monitor.id}" ]
@@ -101,11 +151,15 @@ resource "digitalocean_droplet" "monitor" {
   }
 }
 
+# Create node_count separate to-be-monitored nodes, each in a unique region
+# based on the do_node_regions map.
+# A "remote-exec" provisioner is used to ensure SSH access is available before
+# continuing with further provisioning stages.
 resource "digitalocean_droplet" "node" {
   count = "${var.node_count}"
-  image = "${var.do_image}"
   name = "promguard-node-${count.index + 1}"
   region = "${var.do_node_regions["promguard-node-${count.index +1 }"]}"
+  image = "${var.do_image}"
   size = "${var.do_size}"
   ssh_keys = [ "${digitalocean_ssh_key.promguard-ssh.id}" ]
   tags = [ "${digitalocean_tag.promguard.id}", "${digitalocean_tag.promguard_node.id}" ]
